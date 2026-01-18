5562a8418abf4f8d30a857ddc38a006967809d11
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2026-01-18 16:24:36 +0800

    tut1_4 optimized

--- tut/tut1_4.c
+++ tut/tut1_4.c
@@ -2,18 +2,14 @@
 
 int main() {
       float sol = 1;
+      float prev = 1;
       float x;
       printf("Enter x:\n");
       scanf("%f", &x);
 
       for (int i = 1; i <= 10; i++) {
-          float numerator = 1;
-          int denominator = 1;
-          for (int j = i; j >= 1; j--) {
-              numerator *= x;
-              denominator *= j;
-          }
-          sol += numerator / denominator;
+          prev *= x / i;
+          sol += prev;
       }
       
       printf("Result = %.2f", sol);
