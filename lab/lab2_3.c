4df0a7127649f3184df223b9d6e40983c739db74
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2026-01-25 17:46:49 +0800

    lab2 done

---
+++ lab/lab2_3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int square1(int num);
+void square2(int num, int *result);
+
+int main() {
+  int number, result = 0;
+  printf("Enter the number: \n");
+  scanf("%d", &number);
+  printf("square1(): %d\n", square1(number));
+  square2(number, &result);
+  printf("square2(): %d\n", result);
+  return 0;
+}
+
+int square1(int num) {
+  int i;
+  int sol = 0;
+
+  for (i = 1; i <= num; i++) {
+    sol += (2 * i) - 1;
+  }
+
+  return sol;
+}
+
+void square2(int num, int *result) { *result = square1(num); }
