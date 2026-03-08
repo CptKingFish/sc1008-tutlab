f0b7b8df100cdcf903c402bc125089f990265e8b
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2026-03-08 12:37:55 +0800

    tut6 done

---
+++ tut/tut6_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SIZE 20
+int rCountArray(int array[], int n, int a);
+int main()
+{
+    int array[SIZE];
+    int index, count, target, size;
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d numbers: \n", size);
+    for (index = 0; index < size; index++)
+        scanf("%d", &array[index]);
+    printf("Enter the target number: \n");
+    scanf("%d", &target);
+    count = rCountArray(array, size, target);
+    printf("rCountArray(): %d\n", count);
+    return 0;
+}
+int rCountArray(int array[], int n, int a)
+{
+    if (n == 0)
+        return 0;
+    int isA = (array[0] == a) ? 1 : 0;
+    return isA + rCountArray(array + 1, n - 1, a);
+}
